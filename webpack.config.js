65c89df31b3aedf21859b30feb8c5e8b8c9b2238
Julia Ivanova <julia_sji85@mail.ru>
2020-08-14 23:24:49 +0300

    fixed webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,40 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-  entry: { main: './src/index.js' },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js',
+  entry: {
+    main: './src/index.js',
+    // savedArticles: './saved-articles/index.js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: { loader: 'babel-loader' },
-        exclude: '/node_modules/',
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2,
-            },
-          }, 'postcss-loader'],
-      },
-      {
-        test: /\.(gif|ico|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader?name=./src/images/[name].[ext]',
-          {
-            loader: 'image-webpack-loader',
-            options: {},
-          },
-        ],
-      },
-      {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=./vendor/[name].[ext]',
-      },
-    ],
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      images: path.resolve(__dirname, 'src/images/'),
+    },
   },
   plugins: [
-    new MiniCssExtractPlugin({ filename: 'style.[contenthash].css' }),
-    new OptimizeCssAssetsPlugin({
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/images/favicon.png'),
+          to: path.resolve(__dirname, 'dist/images'),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+    }),
+    new OptimizeCssAssetWebpackPlugin({
       assetNameRegExp: /\.css$/g,
       // eslint-disable-next-line global-require
       cssProcessor: require('cssnano'),
@@ -54,12 +43,47 @@ module.exports = {
       },
       canPrint: true,
     }),
-    new HtmlWebpackPlugin({
-      inject: false,
-      hash: true,
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
     new WebpackMd5Hash(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+            },
+          },
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        use: [
+          'file-loader?name=./images/[name].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {},
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader?name=./vendor/[name].[ext]'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+        }],
+      },
+    ],
+  },
 };
