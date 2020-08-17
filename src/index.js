5baaedd939092dfc437b6580d4e1bc8f8795cd7c
Julia Ivanova <julia_sji85@mail.ru>
2020-08-18 01:19:21 +0300

    added popup login

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './styles/styles.css';
+
+const loginButton = document.querySelector('.menu__btn-login');
+const closeButton = document.querySelector('.popup__close');
+const loginForm = document.querySelector('#form-login');
+
+// Открытие попапа
+function popupOpen(popup) {
+  popup.classList.add('popup_is-opened');
+}
+// Закрытие попапа
+function popupClose(event) {
+  event.target.closest('.popup').classList.remove('popup_is-opened');
+}
+
+loginButton.addEventListener('click', () => {
+  popupOpen(document.querySelector('#popup-login'));
+});
+
+closeButton.addEventListener('click', popupClose);
+loginForm.addEventListener('submit', (event) => event.preventDefault());
