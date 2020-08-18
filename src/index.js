2c67086a7b3a877e5a16ed78cf353f30be1cf8ba
Julia Ivanova <julia_sji85@mail.ru>
2020-08-18 17:29:04 +0300

    added popup Successful registration

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const signupButton = loginForm.querySelector('.popup__or-button');
 const loginButton = signupForm.querySelector('.popup__or-button');
 const popupLogin = document.querySelector('#popup-login');
 const popupSignup = document.querySelector('#popup-signup');
+const popupSuccess = document.querySelector('#popup-success');
 
 /* Функции */
 
@@ -32,6 +33,14 @@ authorizationButton.addEventListener('click', () => {
 closeButtons.forEach((el) => el.addEventListener('click', popupClose));
 loginForm.addEventListener('submit', (event) => event.preventDefault());
 signupForm.addEventListener('submit', (event) => event.preventDefault());
+signupForm.querySelector('.popup__button').addEventListener('click', (event) => {
+  popupClose(event);
+  popupOpen(popupSuccess);
+});
+popupSuccess.querySelector('.popup__login-button').addEventListener('click', (event) => {
+  popupClose(event);
+  popupOpen(popupLogin);
+});
 signupButton.addEventListener('click', (event) => {
   popupClose(event);
   popupOpen(popupSignup);
