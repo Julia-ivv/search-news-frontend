8c11131bab2b984b993c6001e1da4a18c7b71dae
Julia Ivanova <julia_sji85@mail.ru>
2020-08-18 16:22:43 +0300

    added popup signup

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 import './styles/styles.css';
 
-const loginButton = document.querySelector('.menu__btn-login');
-const closeButton = document.querySelector('.popup__close');
+/* Переменные */
+
+const authorizationButton = document.querySelector('.menu__btn-login');
+const closeButtons = document.querySelectorAll('.popup__close');
 const loginForm = document.querySelector('#form-login');
+const signupForm = document.querySelector('#form-signup');
+const signupButton = loginForm.querySelector('.popup__or-button');
+const loginButton = signupForm.querySelector('.popup__or-button');
+const popupLogin = document.querySelector('#popup-login');
+const popupSignup = document.querySelector('#popup-signup');
+
+/* Функции */
 
 // Открытие попапа
 function popupOpen(popup) {
   popup.classList.add('popup_is-opened');
 }
+
 // Закрытие попапа
 function popupClose(event) {
   event.target.closest('.popup').classList.remove('popup_is-opened');
 }
 
-loginButton.addEventListener('click', () => {
+authorizationButton.addEventListener('click', () => {
   popupOpen(document.querySelector('#popup-login'));
 });
 
-closeButton.addEventListener('click', popupClose);
+/* Слушатели событий */
+
+closeButtons.forEach((el) => el.addEventListener('click', popupClose));
 loginForm.addEventListener('submit', (event) => event.preventDefault());
+signupForm.addEventListener('submit', (event) => event.preventDefault());
+signupButton.addEventListener('click', (event) => {
+  popupClose(event);
+  popupOpen(popupSignup);
+});
+loginButton.addEventListener('click', (event) => {
+  popupClose(event);
+  popupOpen(popupLogin);
+});
